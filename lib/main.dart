fc9fd00bf6f8f6076ebc008cadf45f275fd2bff3
walter <walterlopez2004@unitec.edu>
2025-06-21 18:44:44 -0600

    Carpetas creadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'pages/login.dart'; 
-import 'firebase_options.dart';
-import 'utility/theme_provider.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => ThemeProvider(),
-      child: const MyApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeProvider, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            brightness: themeProvider.isDarkMode
-                ? Brightness.dark
-                : Brightness.light,
-            colorSchemeSeed: themeProvider.primaryColor,
-            useMaterial3: true,
-          ),
-          home: const LoginPage(), // o tu página inicial
-        );
-      },
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
